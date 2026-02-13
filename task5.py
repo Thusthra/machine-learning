510b4604c19d6d724ccf715c4be66f669fd1b00f
Thusthra <148706323+Thusthra@users.noreply.github.com>
2026-02-13 14:28:13 +0900

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,29 @@
+a=int(input("Enter your number1: "))
+b=int(input("Enter your number2: "))
+c=int(input("Enter your number3: "))
+
+for i in range(1):
+    if a > b:
+      print ("a is greater")
+    elif b > c:
+        print("b is greater")
+    elif c > a:
+      print("c is greater")
+    else:
+        print("all are equal")
+    
+
+
+a=int(input("Enter your number1: "))
+b=int(input("Enter your number2: "))
+
+for i in range(1):
+    if a > b:
+      print ("a is greater")
+   
+    else:
+        print("b is greater")
+    
+
+for i in range(1,24,3):
+  print(i)
